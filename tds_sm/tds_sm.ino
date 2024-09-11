9874842e0169d0400a45fc93f981fd7d46f74a6c
dardarrr <117033060+dardarrr@users.noreply.github.com>
2024-09-11 07:31:04 +0700

    Add files via upload

---
+++ tds_sm/tds_sm.ino
@@ -0,0 +1,101 @@
+
+#include <ESP8266WiFi.h>
+
+#include <Blynk.h>
+#define BLYNK_TEMPLATE_ID "TMPL6fiMaMNBJ"
+#define BLYNK_DEVICE_NAME "KONTROL NUTRISI"
+#define BLYNK_PRINT Serial
+
+#include <BlynkSimpleEsp8266.h>
+#define TdsSensorPin A0
+#define VREF 3.3              // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30
+
+#define BLYNK_AUTH_TOKEN "O8o97pdnQ0adwzj5LQ20kO7KKNXGtlmS"
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "CATTLEYA2";
+char pass[] = "junong02";           // sum of sample point
+
+int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+
+float averageVoltage = 0;
+float tdsValue = 0;
+float temperature = 23;       // current temperature for compensation
+
+// median filtering algorithm
+int getMedianNum(int bArray[], int iFilterLen){
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+  bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0){
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  }
+  else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
+}
+BLYNK_WRITE(V0) {
+  digitalWrite (A0, param.asInt());
+}
+void setup(){
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud.com", 8080);
+  pinMode(TdsSensorPin,INPUT);
+  pinMode(D8, OUTPUT);
+ 
+}
+
+void loop(){
+  Blynk.run();
+  static unsigned long analogSampleTimepoint = millis();
+  if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT){ 
+      analogBufferIndex = 0;
+    }
+  }   
+  
+  static unsigned long printTimepoint = millis();
+  if(millis()-printTimepoint > 800U){
+    printTimepoint = millis();
+    for(copyIndex=0; copyIndex<SCOUNT; copyIndex++){
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+      
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0;
+      
+      //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0)); 
+      float compensationCoefficient = 1.0+0.02*(temperature-25.0);
+      //temperature compensation
+      float compensationVoltage=averageVoltage/compensationCoefficient;
+      
+      //convert voltage value to tds value
+      tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
+      
+      //Serial.print("voltage:");
+      //Serial.print(averageVoltage,2);
+      //Serial.print("V   ");
+      Serial.print("TDS Value:");
+      Serial.print(tdsValue,0);
+      Serial.println("ppm");
+      Blynk.virtualWrite(V0, tdsValue);
+    }
+  }
+}
